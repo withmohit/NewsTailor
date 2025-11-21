d7ea6c9d6314cca3e21f31d10644e142bf327d54
Mohit K Prajapati <mohit.2003prajapati@gmail.com>
2025-11-21 15:27:42 +0530

    new categories

--- data_pipeline/classification.py
+++ data_pipeline/classification.py
@@ -6,7 +6,7 @@ classifier = pipeline(
     model="valhalla/distilbart-mnli-12-1"
 )
 
-candidate_labels = ["tech_and_science", "health", "india", "politics", "religion"]
+candidate_labels = ["tech_and_science", "crime", "india", "politics", "religion", "india"]
 
 def predict_category(sequence_to_classify):
     result = classifier(sequence_to_classify, candidate_labels)
